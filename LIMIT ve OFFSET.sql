4d68971a4417f77c0ec3e07200b3aec72105a283
Mehmet Eren Kara <mehmeterenkarajob@hotmail.com>
2022-06-08 11:34:36 +0300

    Create LIMIT ve OFFSET.sql

---
+++ LIMIT ve OFFSET.sql
@@ -0,0 +1,24 @@
+--Merhabalar,
+
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+--customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+--Kolay Gelsin.
+
+SELECT title FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+SELECT title FROM film
+WHERE title LIKE '%n'
+ORDER BY length
+OFFSET 5
+LIMIT 5;
+
+SELECT last_name FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
